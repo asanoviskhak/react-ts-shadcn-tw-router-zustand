b17b5059bf8312d9690b00ac9136c838acab6665
Iskhak Asanov <asanov.personal@gmail.com>
2025-10-05 20:33:53 +0600

    added react router

---
+++ src/routes/index.tsx
@@ -0,0 +1,8 @@
+import { Route, Routes } from "react-router";
+import App from "../App";
+
+export const AppRoutes = () => {
+  return (<Routes>
+    <Route path="/" element={<App />} />
+  </Routes>)
+}
