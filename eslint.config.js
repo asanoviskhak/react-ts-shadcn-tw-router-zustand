4b20a551acf701722348c0e733c73107a49c183f
Iskhak Asanov <asanov.personal@gmail.com>
2025-10-05 20:40:54 +0600

    edited eslint

--- eslint.config.js
+++ eslint.config.js
@@ -20,10 +20,10 @@ export default defineConfig([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
-    },
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
     // specify used plugins
     plugins: {
